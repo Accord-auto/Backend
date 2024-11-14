f84787e09bd3a2997f7cb676675185fd443267dc
lasndnansdl <flihjkcey@gmail.com>
2024-11-14 23:49:52 +0500

    fix tests

--- src/test/java/avto/accord/AccordApplicationTests.java
+++ src/test/java/avto/accord/AccordApplicationTests.java
@@ -1,9 +1,5 @@
 package avto.accord;
 
-import avto.accord.App.Web.Controllers.ArticleController.ArticleControllerTest;
-import avto.accord.App.Web.Controllers.CategoryController.CategoryControllerTest;
-import avto.accord.App.Web.Controllers.ProductController.ProductControllerTest;
-import avto.accord.App.Web.Controllers.PropertyController.PropertyControllerTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,45 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AccordApplicationTests {
     @Test
     void contextLoads() {
-    }
-    @Test
-    void testProductController() throws Exception {
-        ProductControllerTest productControllerTest = new ProductControllerTest();
-        productControllerTest.setUp();
-        productControllerTest.testGetAllProducts();
-        productControllerTest.testGetProductById();
-        productControllerTest.testCreateProduct();
-        productControllerTest.testUpdateProductPrice();
-        productControllerTest.testUpdateProductDiscount();
-        productControllerTest.testUpdateProductCount();
-        productControllerTest.testDeleteProduct();
-    }
-
-    @Test
-    void testCategoryController() throws Exception {
-        CategoryControllerTest categoryControllerTest = new CategoryControllerTest();
-        categoryControllerTest.setUp();
-        categoryControllerTest.testAddCategory();
-        categoryControllerTest.testDeleteCategory();
-        categoryControllerTest.testGetAllCategories();
-    }
-
-    @Test
-    void testPropertyController() throws Exception {
-        PropertyControllerTest propertyControllerTest = new PropertyControllerTest();
-        propertyControllerTest.setUp();
-        propertyControllerTest.testAddProperty();
-        propertyControllerTest.testDeleteProperty();
-    }
-
-    @Test
-    void testArticleController() throws Exception {
-        ArticleControllerTest articleControllerTest = new ArticleControllerTest();
-        articleControllerTest.setUp();
-        articleControllerTest.testDeleteArticle();
-        articleControllerTest.testGetAllArticles();
-        articleControllerTest.testGetArticleById();
-        articleControllerTest.testCreateArticle();
-        articleControllerTest.testUpdateArticle();
     }
 }
