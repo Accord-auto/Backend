172190b4403836818d7e125ffb68583e34292f6a
lasndnansdl <flihjkcey@gmail.com>
2024-11-16 22:13:50 +0500

    add photo renaming by uuid

---
+++ src/main/java/avto/accord/App/Domain/Services/PhotoService/PhotoUtils.java
@@ -0,0 +1,30 @@
+package avto.accord.App.Domain.Services.PhotoService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class PhotoUtils {
+    private final PhotoStorage photoStorage;
+
+    @Autowired
+    public PhotoUtils(PhotoStorage photoStorage) {
+        this.photoStorage = photoStorage;
+    }
+
+    public String savePhoto(MultipartFile photo) throws IOException {
+        String photoPath = UUID.randomUUID().toString() + getFileExtension(photo.getOriginalFilename());
+        photoStorage.savePhoto(photo, photoPath);
+        return photoPath;
+    }
+
+    public static String getFileExtension(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf('.');
+        return (lastDotIndex == -1) ? "" : fileName.substring(lastDotIndex);
+    }
+}
