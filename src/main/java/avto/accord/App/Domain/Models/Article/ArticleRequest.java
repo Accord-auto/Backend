7de785ddc1f8bc2226964b1164a943aad1ec4224
Минаков Эдуард <133601457+Binarer@users.noreply.github.com>
2025-01-07 16:40:32 +0500

    Update ArticleRequest.java

--- src/main/java/avto/accord/App/Domain/Models/Article/ArticleRequest.java
+++ src/main/java/avto/accord/App/Domain/Models/Article/ArticleRequest.java
@@ -2,10 +2,14 @@ package avto.accord.App.Domain.Models.Article;
 
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class ArticleRequest {
     private String title;
+    @NotBlank(message = "Description is mandatory")
+    @Size(max = 1000, message = "Description must be less than 1000 characters")
     private String description;
 }
