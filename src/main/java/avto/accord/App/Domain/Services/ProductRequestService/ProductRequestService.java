47180907bdf69925389042a3289ec62d47c2e481
lasndnansdl <flihjkcey@gmail.com>
2025-01-08 14:49:40 +0500

    log mapper ProductRequestService

--- src/main/java/avto/accord/App/Domain/Services/ProductRequestService/ProductRequestService.java
+++ src/main/java/avto/accord/App/Domain/Services/ProductRequestService/ProductRequestService.java
@@ -7,6 +7,7 @@ import avto.accord.App.Domain.Models.Product.ProductRequestPayload;
 import avto.accord.App.Infrastructure.Components.Mapper.ProductRequestMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,19 +15,24 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProductRequestService {
     @Autowired
     private IProductService productService;
 
-    public Product createProduct(MultipartFile mainPhoto, List<MultipartFile> additionalPhotos, String productRequestPayloadJson) throws IOException, JsonProcessingException {
+    public Product createProduct(MultipartFile mainPhoto, List<MultipartFile> additionalPhotos, String productRequestPayloadJson) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        ProductRequestPayload productRequestPayload = objectMapper.readValue(productRequestPayloadJson, ProductRequestPayload.class);
+        try{
+            ProductRequestPayload productRequestPayload = objectMapper.readValue(productRequestPayloadJson, ProductRequestPayload.class);
+            log.info("Mapping ProductRequestPayload to ProductRequest");
+            ProductRequest productRequest = ProductRequestMapper.INSTANCE.toProductRequest(productRequestPayload);
+            productRequest.setMainPhoto(mainPhoto);
+            productRequest.setAdditionalPhotos(additionalPhotos);
 
-        ProductRequest productRequest = ProductRequestMapper.INSTANCE.toProductRequest(productRequestPayload);
-        productRequest.setMainPhoto(mainPhoto);
-        productRequest.setAdditionalPhotos(additionalPhotos);
-
-        return productService.saveProduct(productRequest);
+            return productService.saveProduct(productRequest);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
