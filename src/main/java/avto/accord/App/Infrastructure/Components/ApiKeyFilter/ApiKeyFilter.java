11ede4b8ee2ddbbd5713c4bcb47495712c92a2d0
lasndnansdl <flihjkcey@gmail.com>
2025-04-05 22:33:08 +0500

    fix

--- src/main/java/avto/accord/App/Infrastructure/Components/ApiKeyFilter/ApiKeyFilter.java
+++ src/main/java/avto/accord/App/Infrastructure/Components/ApiKeyFilter/ApiKeyFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Collections;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ApiKeyFilter extends OncePerRequestFilter {
@@ -26,8 +28,10 @@ public class ApiKeyFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         String path = request.getRequestURI();
+        log.info("Processing request for path: {}", path);
 
         if (path.startsWith("/swagger-ui/") || path.startsWith("/v3/api-docs") || path.startsWith("/v3-docs")) {
+            log.debug("Skipping API key validation for path: {}", path);
             filterChain.doFilter(request, response);
             return;
         }
@@ -35,17 +39,21 @@ public class ApiKeyFilter extends OncePerRequestFilter {
         // Проверяем наличие API-ключа в заголовке
         String apiKey = request.getHeader("X-API-KEY");
         if (apiKey == null) {
+            log.warn("Missing X-API-KEY header for path: {}", path);
             filterChain.doFilter(request, response);
             return;
         }
 
+        log.info("Validating API key: {}", apiKey);
         if (!apiKeyService.validateApiKey(apiKey)) {
+            log.warn("Invalid or expired API key: {}", apiKey);
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write("Invalid or expired API key");
             return;
         }
 
         apiKeyService.getRoleForApiKey(apiKey).ifPresent(role -> {
+            log.info("Setting role in security context: {}", role);
             UsernamePasswordAuthenticationToken authentication =
                     new UsernamePasswordAuthenticationToken(null, null, Collections.singletonList(new SimpleGrantedAuthority(role)));
             SecurityContextHolder.getContext().setAuthentication(authentication);
